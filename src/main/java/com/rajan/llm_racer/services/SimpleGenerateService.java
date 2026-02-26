b93cd5b7d103b0aaf13fe5853051b1efc487a998
rajan chauhan <rajanchauhan@rajans-MacBook-Pro.local>
2026-02-26 21:21:47 +0530

    Handled exceptions

--- src/main/java/com/rajan/llm_racer/services/SimpleGenerateService.java
+++ src/main/java/com/rajan/llm_racer/services/SimpleGenerateService.java
@@ -2,15 +2,16 @@ package com.rajan.llm_racer.services;
 
 import com.rajan.llm_racer.models.GenerateRequest;
 import com.rajan.llm_racer.provider.LLMProvider;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.StructuredTaskScope;
 import java.util.concurrent.TimeoutException;
 
+@Slf4j
 @Service
 public class SimpleGenerateService implements GenerateService {
     private final LLMProvider llmProviderA, llmProviderB, llmProviderC;
@@ -26,17 +27,19 @@ public class SimpleGenerateService implements GenerateService {
 
     @Override
     public String generate(GenerateRequest req) {
+        log.info("Generate request: {}", req);
         try {
             return ScopedValue.where(PROMPT, req.prompt()).where(ORG_ID, req.orgId()).call(() -> {
                 try (var scope = new StructuredTaskScope.ShutdownOnSuccess<String>()) {
-                    scope.fork(() -> llmProviderA.generate(PROMPT.get(), ORG_ID.get()));
-                    scope.fork(() -> llmProviderB.generate(PROMPT.get(), ORG_ID.get()));
-                    scope.fork(() -> llmProviderC.generate(PROMPT.get(), ORG_ID.get()));
+                    var task1 = scope.fork(() -> llmProviderA.generate(PROMPT.get(), ORG_ID.get()));
+                    var task2 = scope.fork(() -> llmProviderB.generate(PROMPT.get(), ORG_ID.get()));
+                    var task3 = scope.fork(() -> llmProviderC.generate(PROMPT.get(), ORG_ID.get()));
                     var deadline = Instant.now().plusSeconds(req.timeoutSeconds());
                     scope.joinUntil(deadline);
                     return scope.result();
-                } catch (InterruptedException | ExecutionException ex) {
-                    throw new RuntimeException(ex);
+                } catch (InterruptedException ex) {
+                    Thread.currentThread().interrupt();
+                    throw ex;
                 } catch (TimeoutException e) {
                     throw new RuntimeException("Timeout exceeded", e);
                 }
@@ -44,6 +47,5 @@ public class SimpleGenerateService implements GenerateService {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-
     }
 }
